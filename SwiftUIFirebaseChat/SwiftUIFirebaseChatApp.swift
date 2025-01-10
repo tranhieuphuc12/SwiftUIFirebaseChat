e77410729cafdb35772e58cca50535628ad369cb
PhucTran <tranhieuphuc12@gmail.com>
2025-01-10 09:37:50 +0700

    Initial Commit

---
+++ SwiftUIFirebaseChat/SwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseChatApp.swift
+//  SwiftUIFirebaseChat
+//
+//  Created by DaiTran on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIFirebaseChatTests/SwiftUIFirebaseChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseChatTests.swift
+//  SwiftUIFirebaseChatTests
+//
+//  Created by DaiTran on 10/1/25.
+//
+
+import Testing
+@testable import SwiftUIFirebaseChat
+
+struct SwiftUIFirebaseChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
